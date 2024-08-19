b87d6269de34bb672335c7a52090e78556c76525
lostune <1165034964@qq.com>
2024-08-19 17:52:59 +0800

    Site updated: 2024-08-19 17:52:58

--- js/essay.js
+++ js/essay.js
@@ -4,6 +4,19 @@ function whenDOMReady() {
   whenDOMReady()
   document.addEventListener("pjax:complete", whenDOMReady)
   
+  function initEssay() {
+    if (document.querySelector('#essay-mini')) {
+      let swiper = new Swiper('.swiper-container', {
+          direction: 'vertical',
+          loop: true,
+          autoplay: {
+              delay: 3000,
+              pauseOnMouseEnter: true
+          },
+      });
+    }
+  } // Swiper轮播 - 即刻mini
+  
   // 适配pjax
   
   window.onresize = () => {
