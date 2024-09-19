a5a29dbc369e1bd8c3cbb741ca3ba0dad8b0d14a
lostune <1165034964@qq.com>
2024-09-20 00:03:51 +0800

    Site updated: 2024-09-20 00:03:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/14/秋招经历/","2024/09/04/常用数学公式/","2024/08/29/Hello-World/"];function toRandomPost(){
+var posts=["2024/09/14/秋招经历/","2024/09/04/常用数学公式/","2024/08/29/Hello-World/","2024/09/19/大学几年微信好友和邮箱的内容/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
