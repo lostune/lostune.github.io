588bd79f57755a19207928df16546b3cbcde8bff
nightvoyageryhx <1907507811@qq.com>
2025-09-22 00:12:15 +0800

    Site updated: 2025-09-22 00:12:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/14/秋招经历/","2024/09/04/常用数学公式/","2024/08/29/Hello-World/","2024/10/24/摘抄/","2025/01/11/anaconda、tensorflow和pytorch的配置和使用问题/"];function toRandomPost(){
+var posts=["2024/09/14/秋招经历/","2024/09/04/常用数学公式/","2024/08/29/Hello-World/","2024/10/24/摘抄/","2025/01/11/anaconda、tensorflow和pytorch的配置和使用问题/","2025/09/21/关于林夕/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
